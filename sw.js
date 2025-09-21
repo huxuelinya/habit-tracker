ec6b75ccc9b810bf46643849d91e024beab7cc22
huxuelinya <huxuelin@gmail.com>
2025-09-21 22:43:36 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,14 @@
-self.addEventListener('install',()=>self.skipWaiting());self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+const CACHE = 'habit-cache-v2';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll([
+    './','./index.html','./manifest.webmanifest',
+    './icons/icon-192.png','./icons/icon-512.png'
+  ])));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
